d5f50a0176f649f2b83217376d8b1f840cb4d631
Brendan Kelly <brewkelly@gmail.com>
2024-03-05 22:56:25 -0500

    Render basic visualization
    First milestone!

--- main.cpp
+++ main.cpp
@@ -11,13 +11,9 @@
 #include "Music.h"
 #include "Renderer.h"
 #include "Shader.h"
-#include "VertexArray.h"
-#include "VertexAttributes.h"
 #include "SoundData.h"
-#include "IndexBuffer.h"
 
 const unsigned int dataBlockSize = 16384;
-
 
 #define TRY(expression, returnValue)                                        \
     ({                                                                      \
@@ -62,36 +58,14 @@ int main()
     auto data = TRY(SoundData::create("/home/brendan/dev/music-visualizer/hoty.wav"), 1);
     auto samplerate = data.info().samplerate;
 
-    Shader shaderProgram;
-    if (!shaderProgram.load("/home/brendan/dev/music-visualizer/vertex-shader.glsl", "/home/brendan/dev/music-visualizer/fragment-shader.glsl")) {
-        std::cout << "Failed to load shader program\n";
-        return 1;
-    }
-
-    float vertices[] = {
-        1.0f,  1.0f, 0.0f,   // top right
-        1.0f, -1.0, 0.0f,    // bottom right
-        -1.0f, -1.0f, 0.0f,  // bottom left
-        -1.0f,  1.0f, 0.0f  // top left
-    };
-    unsigned int indices[] = {
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    VertexArray vertexArray;
-    VertexAttributes vertexAttributes;
-    VertexBuffer vertexBuffer(vertices, sizeof(vertices));
-    IndexBuffer indexBuffer(indices, 6);
-
-    vertexAttributes.push<float>(3);
-    vertexAttributes.enable(vertexBuffer, vertexArray);
 
     auto windowPtr = window.ptr();
     auto* in = fftw_alloc_complex(dataBlockSize);
     auto* out = fftw_alloc_complex(dataBlockSize);
     double lastTime = 0.0f;
     size_t numFrames = 0;
+    size_t dataIndex = 0;
+    Renderer renderer;
     while (!glfwWindowShouldClose(windowPtr)) {
         auto currentTime = glfwGetTime();
         auto deltaTime = currentTime - lastTime;
@@ -104,45 +78,39 @@ int main()
             Mix_PlayMusic(music.ptr(), -1);
         }
 
-        std::cout << "deltaTime: " << deltaTime << '\n';
-        auto samplesSinceLastFrame = samplerate * deltaTime;
-        std::cout << "samplesSinceLastFrame: " << samplesSinceLastFrame << '\n';
-        // Round samplesPerFrame up to the nearest power of 2
-        auto power = std::ceil(std::log2(samplesSinceLastFrame));
+        auto samplesSinceLastFrame = static_cast<size_t>(samplerate * deltaTime);
+        // Round samplesPerFrame down to the nearest power of 2
+        auto power = std::floor(std::log2(samplesSinceLastFrame));
         auto samplesPerFramePowerOf2 = static_cast<size_t>(std::pow(2, power));
-        std::cout << "samplesPerFrame (rounded up to nearest power of 2): " << samplesPerFramePowerOf2 << '\n';
 
-        std::cout << "Before: ";
         for (size_t i = 0; i < samplesPerFramePowerOf2; ++i) {
-            auto dataIndex = i + numFrames * samplesPerFramePowerOf2;
             in[i][0] = data.at(dataIndex);
             in[i][1] = 0;
-            std::cout << "{ real: " << in[i][0] << ", complex: " << in[i][1] << " } ";
+            dataIndex++;
         }
-        std::cout << "\n------------------------\n";
 
-        auto* p = fftw_plan_dft_1d(dataBlockSize, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-        double magnitudes[dataBlockSize];
-        std::cout << "After: ";
-        for (size_t i = 0; i < samplesPerFramePowerOf2; ++i) {
-            magnitudes[i] = std::sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
-            std::cout << "{ real: " << out[i][0] << ", complex: " << out[i][1] << ", magnitude: " << magnitudes[i] << " } ";
-        }
-        std::cout << "\n------------------------\n";
-
+        fftw_plan_s* p = fftw_plan_dft_1d(static_cast<int>(samplesPerFramePowerOf2), in, out, FFTW_FORWARD, FFTW_ESTIMATE);
         fftw_execute(p);
+
+        const size_t numSamplesShown = 100;
+        double magnitudes[numSamplesShown];
+        for (size_t i = 0; i < numSamplesShown; ++i) {
+            magnitudes[i] = std::sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
+        }
+
         fftw_destroy_plan(p);
 
         int width;
         int height;
         glfwGetWindowSize(windowPtr, &width, &height);
         auto resolution = glm::vec2(width, height);
-        shaderProgram.setUniform("uTime", currentTime);
-        shaderProgram.setUniform<glm::vec2>("uResolution", resolution);
-        shaderProgram.use();
         Renderer::clear();
-        Renderer::drawElements(6);
+        auto rectangleWidth = 1 / static_cast<double>(numSamplesShown);
+        for (size_t i = 0; i < numSamplesShown; ++i) {
+            auto rectangleHeight = magnitudes[i] * 0.01;
+            auto bottomLeftPosition = glm::vec2((static_cast<double>(i) * rectangleWidth - 0.5) * 2.0f, -1);
+            renderer.drawRectangle(rectangleWidth, rectangleHeight, bottomLeftPosition);
+        }
 
         glfwPollEvents();
         glfwSwapBuffers(windowPtr);
