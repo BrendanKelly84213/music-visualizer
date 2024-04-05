78d91c2e26b11320e73ff74c93c270ead2c7ebe4
Brendan Kelly <brewkelly@gmail.com>
2024-04-05 14:20:40 -0400

    Refactor hard coded project root to macro in config.h

--- PerlinNoise.cpp
+++ PerlinNoise.cpp
@@ -3,12 +3,14 @@
 //
 
 #include "PerlinNoise.h"
+#include "config.h"
 
 std::shared_ptr<PerlinNoise> PerlinNoise::create()
 {
     auto instance = std::make_shared<PerlinNoise>();
     instance->m_shader = Shader::create();
-    std::string root = "/home/brendan/dev/my-stuff/music-visualizer/assets/shaders/";
+
+    std::string root = LOCAL_PATH("assets/shaders/");
     instance->m_vertexShaderPath = root + "vertex-shader.glsl";
     instance->m_fragmentShaderPath = root + "smoke-fragment-shader.glsl";
     return instance;
