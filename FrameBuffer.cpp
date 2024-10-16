c9b8f72254944fbae025ecc8e120dd63b6473d2a
Brendan Kelly <brewkelly@gmail.com>
2024-10-16 17:17:05 -0400

    Add FrameBuffer class

---
+++ FrameBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brendan on 10/16/24.
+//
+
+#include "FrameBuffer.h"
---
+++ FrameBuffer.h
@@ -0,0 +1,14 @@
+//
+// Created by brendan on 10/16/24.
+//
+
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
+
+
+class FrameBuffer {
+
+};
+
+
+#endif //FRAMEBUFFER_H
